150647190b136c590beda20d28b89725757b12fb
MohamedHanafiAlaoui <hanafimohamed510@gmail.com>
2026-01-24 17:37:13 +0100

    Refactor TrackRepository to replace artist and category methods with paginated search by title and category

--- MusicStream/src/main/java/com/example/MusicStream/repository/TrackRepository.java
+++ MusicStream/src/main/java/com/example/MusicStream/repository/TrackRepository.java
@@ -2,15 +2,15 @@ package com.example.MusicStream.repository;
 
 import com.example.MusicStream.entity.MusicCategory;
 import com.example.MusicStream.entity.Track;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
+
 
 public interface TrackRepository extends JpaRepository<Track,Long> {
-    List<Track> findByArtist(String artist);
 
-    List<Track> findByCategory(MusicCategory category);
 
-    List<Track> findByTitleContainingIgnoreCase(String title);
+    Page<Track> findByTitleContainingIgnoreCaseAndCategory(String title, MusicCategory musicCategory, Pageable pageable);
 
 }
